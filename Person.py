584d135ffdd95b4d567858dabee0206484417fed
Lucio Lopez Plancarte <Lucio.LopezPlancarte@enmu.edu>
2025-09-12 10:32:06 -0600

    Trying to commit with my account

--- Person.py
+++ Person.py
@@ -1,6 +1,10 @@
 #This is the person class
 
 class Person():
-    pass
-
+    def __init__(self, name="Neo", age=21):
+        self.name = name
+        self.age = age
+    
+    def Introduce(self):
+        return (f"Hello my name is {self.name} and I am {self.age}")
 
